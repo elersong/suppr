571360cb25a2c508bc92391050995a74aacfc9df
Grey Elerson <grey.l.elerson@gmail.com>
2021-05-17 22:09:27 -0600

    attempting redeploy

--- back-end/src/reservations/reservations.controller.js
+++ back-end/src/reservations/reservations.controller.js
@@ -38,6 +38,7 @@ const hasValidReservationData = (req, res, next) => {
   const peopleIsValid = typeof people === 'number'
 
   const dateIsValid = (new Date(reservation_date).getDay() !== 1) && (Date.parse(reservation_date) >= Date.now())
+  
   if (dateFormatIsValid && !dateIsValid) {
     message = "Restaurant closed. Date must be any future non-Tuesday."
   }
