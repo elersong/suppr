db4e7563243498df58f48b352a971d5b82ef4036
Grey Elerson <grey.l.elerson@gmail.com>
2021-06-02 15:21:58 -0600

    REFACTOR: SeatingForm component

--- front-end/src/dashboard/SeatingForm.js
+++ front-end/src/dashboard/SeatingForm.js
@@ -1,9 +1,16 @@
-import React, { useState, useEffect } from "react";
-import { useHistory, useParams } from "react-router-dom";
-import { seatTable, listTables } from "../utils/api";
 import ErrorAlert from "../layout/ErrorAlert";
+import React, { useState, useEffect } from "react";
+import { seatTable, listTables } from "../utils/api";
+import { useHistory, useParams } from "react-router-dom";
 
-function SeatingForm({ date }) {
+/**
+ * Displays the form to seat a reservation at a free table
+ * @param {Number} reservation_id - Optional | The reservation, from the url, to be updated
+ *
+ * @returns {JSX.Element}
+ */
+
+function SeatingForm() {
   const { reservation_id } = useParams();
   const startingValues = {
     table_id: "1",
@@ -18,10 +25,11 @@ function SeatingForm({ date }) {
 
   const history = useHistory();
 
+  // This doesn't require exhaustive dependencies
   // eslint-disable-next-line
-  useEffect(loadData, [date, reservation_id]);
+  useEffect(loadData, [reservation_id]);
 
-  // Look up data for all tables
+  // Look up data for all table objects
   // Look up data for all reservations, find the current one
   function loadData() {
     const abortController = new AbortController();
@@ -38,52 +46,41 @@ function SeatingForm({ date }) {
 
     return () => abortController.abort();
   }
-  console.log("IN SEATINGFORM COMPONENT");
+
   const handleSubmit = (e) => {
     e.preventDefault();
-    e.stopPropagation();
-    console.log("IN THE HANDLESUBMIT");
-    const ABORT = new AbortController();
     setSubmitError(null);
-    console.log("SEATING FORM JS");
     seatTable(formData)
       .then(() => history.push("/dashboard"))
-      .catch((error) => console.log("FORM SUBMIT ERROR", error));
-    return () => {
-      ABORT.abort();
-    };
+      .catch(setSubmitError);
   };
 
   return (
     <div>
       {submitError && <ErrorAlert error={submitError} />}
       {formError && <ErrorAlert error={formError} />}
-    <form onSubmit={handleSubmit}>
-      <label htmlFor="table_id">Select Table:</label>
-        <select
-          id="table_id"
-          name="table_id"
-          >
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="table_id">Select Table:</label>
+        <select id="table_id" name="table_id">
           {tableData.length > 0 &&
             tableData.map((table) => {
               return (
-                <option
-                key={`table-${table.table_id}`}
-                value={table.table_id}
-                >{`${table.table_name} - ${table.capacity}`}</option>
-                );
-              })}
+                <option key={`table-${table.table_id}`} value={table.table_id} >
+                  {`${table.table_name} - ${table.capacity}`}
+                </option>
+              );
+            })}
         </select>
         <br></br>
 
-      <button type="submit" className="btn btn-secondary" id="pleaseClick">
-        Submit
-      </button>
+        <button type="submit" className="btn btn-secondary" id="pleaseClick">
+          Submit
+        </button>
 
-      <button type="button" onClick={history.goBack} className="btn btn-secondary">
+        <button type="button" onClick={history.goBack} className="btn btn-secondary">
           Cancel
         </button>
-    </form>
+      </form>
     </div>
   );
 }
